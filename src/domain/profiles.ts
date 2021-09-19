2d65d7047748177a040052a3b0976c1e23d5916c
xwmtp <xwillmtp@gmail.com>
2021-09-19 15:43:57 +0200

    Add blackout and short profile

--- src/domain/profiles.ts
+++ src/domain/profiles.ts
@@ -1,26 +1,20 @@
 import { Mode } from "./options"
 
 export type Profile = {
-    minimumSynergy: number;
-    maximumSynergy: number;
-    maximumIndividualSynergy: number;
-    maximumSpill: number;
-    initialOffset: number;
-    maximumOffset: number;
-    baselineTime: number;
-    timePerDifficulty: number;
-    tooMuchSynergy: number;
-}
-
-type Profiles = {
-    [key in Mode]: Profile;
+    minimumSynergy: number;             // the minimum synergy allowed in any row
+    maximumSynergy: number;             // the maximum synergy allowed in any row
+    maximumIndividualSynergy: number;   // the maximum synergy allowed between any pair of two goals in a row
+    initialOffset: number;              // the initial allowed deviation from the desired time for a goal
+    maximumOffset: number;              // the maximum allowed deviation from the desired time for a goal
+    baselineTime: number;               // UNUSED in the generator, but used to analyze rows to calculate row durations. It's the base amount of time you'd need in any row to setup.
+    timePerDifficulty: number;          // the ratio between time and difficulty
+    tooMuchSynergy: number;             // high number that is returned in the generator whenever synergy is considered too high
 }
 
 const defaultProfile: Profile = {
     minimumSynergy: -3,
     maximumSynergy: 7,
     maximumIndividualSynergy: 3.75,
-    maximumSpill: 2,
     initialOffset: 1,
     maximumOffset: 2,
     baselineTime: 27.75,
@@ -28,18 +22,24 @@ const defaultProfile: Profile = {
     tooMuchSynergy: 100
 }
 
+type Profiles = {
+    [key in Mode]: Profile;
+}
+
 export const profiles: Profiles = {
-    normal: {
-        minimumSynergy: defaultProfile.minimumSynergy,
-        maximumSynergy: defaultProfile.maximumSynergy,
-        maximumIndividualSynergy: defaultProfile.maximumIndividualSynergy,
-        maximumSpill: defaultProfile.maximumSpill,
-        initialOffset: defaultProfile.initialOffset,
-        maximumOffset: defaultProfile.maximumOffset,
-        baselineTime: defaultProfile.baselineTime,
-        timePerDifficulty: defaultProfile.timePerDifficulty,
-        tooMuchSynergy: defaultProfile.tooMuchSynergy
+    normal: defaultProfile,
+    blackout: {
+        ...defaultProfile,
+        minimumSynergy: -10,
+        maximumSynergy: 10,
+        maximumIndividualSynergy: 4.5,
+        initialOffset: 2,
+        maximumOffset: 6,
     },
-    blackout: defaultProfile,
-    short: defaultProfile,
+    short: {
+        ...defaultProfile,
+        maximumSynergy: 3,
+        baselineTime: 12,
+        timePerDifficulty: 0.5,
+    },
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { BingoList } from "./domain/goalList";
 import { Options } from "./domain/options";
-import { Profile } from "./domain/profiles";
+import { profiles } from "./domain/profiles";
 import BingoGenerator from "./generator";
 import { bingoList } from "./goal-lists/example-goal-list";
-import seedrandom from "./legacy/seedrandom-min";
 import { BingoGenerator as OldBingoGenerator } from "./oldGenerator/generator"
 
 const goalList: BingoList = bingoList;
@@ -15,17 +14,7 @@ const options: Options = {
     language: 'name'
 }
 
-const profile: Profile = {
-    minimumSynergy: -3,
-    maximumSynergy: 7,
-    maximumIndividualSynergy: 3.75,
-    maximumSpill: 2,
-    initialOffset: 1,
-    maximumOffset: 2,
-    baselineTime: 27.75,
-    timePerDifficulty: 0.75,
-    tooMuchSynergy: 100
-}
+const profile = profiles.normal;
 
 
 const getGoalNames = (card) => {
@@ -60,9 +49,6 @@ console.log(getGoalNames(oldCard));
 console.log("\n#############\n TS")
 const bingoGenerator = new BingoGenerator(goalList['normal'], options);
 const card = bingoGenerator.generateCard(1);
-
-
-
 //console.log(card);
 console.log(getGoalNames(card));
 
