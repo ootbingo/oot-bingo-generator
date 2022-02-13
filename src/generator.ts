f34df132b5b531815b6672aa607ece8487e4f763
xwmtp <xwillmtp@gmail.com>
2022-02-13 15:16:45 +0100

    Improve readability of population order generating, code layout improvements

--- src/generator.ts
+++ src/generator.ts
@@ -1,19 +1,19 @@
-import { Card, Square, squarePositions } from "./domain/board";
-import { BingoList, GoalList, Goal } from "./domain/goalList";
-import { Synergies, Synfilters } from "./domain/synergies"
-import { Profile, profiles } from "./domain/profiles";
-import { Options, Mode } from "./domain/options";
-import { INDICES_PER_ROW, Row, ROWS_PER_INDEX } from "./domain/rows";
-import { generateMagicSquare } from "./magicSquare";
-import { RNG } from "./rng";
-import { SynergyCalculator } from "./synergyCalculator";
+import {Card, Square, squarePositions} from "./domain/board";
+import {BingoList, GoalList, Goal} from "./domain/goalList";
+import {Synergies, Synfilters} from "./domain/synergies"
+import {Profile, profiles} from "./domain/profiles";
+import {Options, Mode} from "./domain/options";
+import {INDICES_PER_ROW, Row, ROWS_PER_INDEX} from "./domain/rows";
+import {generateMagicSquare} from "./magicSquare";
+import {RNG} from "./rng";
+import {SynergyCalculator} from "./synergyCalculator";
 
 
 /**
  * Main function for generating boards
  * Function name has to be preserved for compatibility with bingosetup.js in the bingo repo
  * Returns card in the right (legacy) format for the bingo setup
- * @param goalList
+ * @param bingoList
  * @param options
  * @returns A bingo card
  */
@@ -24,8 +24,8 @@ export const ootBingoGenerator = (bingoList: BingoList, options: Options) => {
 
     // make goals start from position 1 in the list (expected by bingosetup.js)
     const shiftedGoals = [];
-    goals.forEach((goal, index) => shiftedGoals[index+1] = goal);
-    
+    goals.forEach((goal, index) => shiftedGoals[index + 1] = goal);
+
     shiftedGoals['meta'] = meta;
     return shiftedGoals;
 };
@@ -53,7 +53,7 @@ export default class BingoGenerator {
 
     constructor(goalList: GoalList, options: Options) {
         this.options = options;
-        
+
         this.rng = new RNG(options.seed);
         this.magicSquare = generateMagicSquare(this.options.seed);
 
@@ -102,7 +102,7 @@ export default class BingoGenerator {
 
         // fill in the goals of the board in a random order
         const populationOrder = this.#generatePopulationOrder(bingoBoard);
-        
+
         for (const i of squarePositions) {
             const nextPosition = populationOrder[i];
 
@@ -120,7 +120,7 @@ export default class BingoGenerator {
 
     /**
      * Pick a goal to fill the specified square on the board. It may not cause too much (anti)synergy in any row.
-     * 
+     *
      * @param position The position of the square
      * @param bingoBoard The bingo board
      * @returns The goal and its synergy or false, if no fitting goal was found
@@ -231,9 +231,9 @@ export default class BingoGenerator {
 
     /**
      * Generates the order in which the squres should be filled by the generator.
-     * 
+     *
      * First the three squres with the highest difficulty, then the center, then the diagonals, then the rest.
-     * 
+     *
      * @param bingoBoard The magic square.
      */
     #generatePopulationOrder(bingoBoard: Square[]): number[] {
@@ -249,14 +249,26 @@ export default class BingoGenerator {
         const nondiagonals = this.#shuffled([1, 2, 3, 5, 7, 9, 10, 11, 13, 14, 15, 17, 19, 21, 22, 23]);
         populationOrder = populationOrder.concat(nondiagonals);
 
-        [23, 24, 25].forEach(difficulty => this.#moveSquareWithDifficultyInFront(difficulty, populationOrder, bingoBoard));
+        this.#movePositionsWithHighestDifficultyToFront(3, populationOrder, bingoBoard);
 
         return populationOrder;
     };
 
-    #moveSquareWithDifficultyInFront(difficulty: number, populationOrder: number[], bingoBoard: Square[]) {
+    #movePositionsWithHighestDifficultyToFront(n: number, populationOrder: number[], bingoBoard: Square[]) {
+        if (n > bingoBoard.length) {
+            n = bingoBoard.length;
+        }
+        const difficulties = bingoBoard.map(square => square.difficulty);
+        const highestDifficulties = difficulties.sort((x, y) => y - x).slice(0, n);
+        const highestDifficultiesAscending = highestDifficulties.sort((x, y) => x - y);
 
-        let currentSquare = bingoBoard.findIndex(square => square.difficulty == difficulty);
+        for (const difficulty of highestDifficultiesAscending) {
+            this.#movePositionWithDifficultyToFront(difficulty, populationOrder, bingoBoard);
+        }
+    }
+
+    #movePositionWithDifficultyToFront(difficulty: number, populationOrder: number[], bingoBoard: Square[]) {
+        const currentSquare = bingoBoard.findIndex(square => square.difficulty == difficulty);
 
         if (currentSquare === -1) {
             // This should never happen
@@ -268,7 +280,7 @@ export default class BingoGenerator {
     }
 
     #shuffled<T>(array: Array<T>): Array<T> {
-        let toShuffle = array.slice();
+        const toShuffle = array.slice();
         for (let i = 0; i < toShuffle.length; i++) {
             const randElement = Math.floor(this.rng.nextRandom() * (i + 1));
             const temp = toShuffle[i];
@@ -282,26 +294,28 @@ export default class BingoGenerator {
     /**
      * Shuffles list of goals, but makes it more likely for goals with lower weights to appear earlier in the list (for frequency balancing).
      * Goal weights should be numbers between -2 and 2.
-     * 
-     * @param a first goal
-     * @param b second goal
+     *
+     * @param goals list of goals
      */
     #weightedShuffle(goals: Goal[]): Goal[] {
         return goals
-            .map(goal => ({ goal, sortVal: (goal.weight || 0) + this.rng.nextRandom() + this.rng.nextRandom() + this.rng.nextRandom() + this.rng.nextRandom() - 2 }))
-            .sort(({ sortVal: sv1 }, { sortVal: sv2 }) => sv2 - sv1)
-            .map(({ goal }) => goal);
+            .map(goal => ({
+                goal,
+                sortVal: (goal.weight || 0) + this.rng.nextRandom() + this.rng.nextRandom() + this.rng.nextRandom() + this.rng.nextRandom() - 2
+            }))
+            .sort(({sortVal: sv1}, {sortVal: sv2}) => sv2 - sv1)
+            .map(({goal}) => goal);
     }
 }
 
 /**
  * Can be used to sort an array of goals first by their time, then by their id ascending.
- * 
+ *
  * @param a first goal
  * @param b second goal
  */
 function sortByTimeAndId(a: Goal, b: Goal): number {
-    var timeDiff = a.time - b.time;
+    const timeDiff = a.time - b.time;
 
     if (timeDiff !== 0) {
         return timeDiff;
@@ -309,18 +323,16 @@ function sortByTimeAndId(a: Goal, b: Goal): number {
 
     if (a.id > b.id) {
         return 1;
-    }
-    else if (a.id < b.id) {
+    } else if (a.id < b.id) {
         return -1;
-    }
-    else {
+    } else {
         return 0;
     }
-};
+}
 
 /**
  * Generate a sorted array of goals from a complex goal list object.
- * 
+ *
  * @param goalList The original goal list.
  */
 function flattenGoalList(goalList: GoalList): Goal[] {
@@ -339,9 +351,9 @@ function flattenGoalList(goalList: GoalList): Goal[] {
 /**
  * Synergy filters are strings that start with 'max' or 'min' followed by a space and a number
  * Examples: 'max -1', 'min 2', 'min -2'
- * 
+ *
  * Splits synergy filter strings into objects with 'max' or 'min' and the numeric value
- * 
+ *
  * @param filters Object with a synergy filter string for several types (e.g. {childchu : 'min 1', endon : 'max -1'})
  * @returns Object with the parsed Synfilters (e.g. {childchu: {minmax: 'min', value: 1}, endon: {'minmax': max}, value: -1})
  */
@@ -363,21 +375,19 @@ function parseSynergyFilters(filters: { [key: string]: string }): Synfilters {
 
 /**
  * Extracts the goal list for a given mode from the full definition of bingo goals.
- * 
+ *
  * @param bingoList The JavaScript object generated from the goal CSV.
  * @param mode The requested mode.
  */
-function extractGoalList(bingoList: BingoList, mode: Mode): GoalList | undefined {
+export function extractGoalList(bingoList: BingoList, mode: Mode): GoalList | undefined {
 
     if (bingoList.info.combined && bingoList.info.combined === "true") {
         const combinedBingoList = bingoList;
         if (combinedBingoList[mode]) {
             return combinedBingoList[mode];
-        }
-        else if (combinedBingoList["normal"]) {
+        } else if (combinedBingoList["normal"]) {
             return combinedBingoList["normal"];
-        }
-        else {
+        } else {
             throw Error(`Goal list doesn't contain a valid sub goal list for mode: "${mode}"`);
         }
     }
